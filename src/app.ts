c38d1c9b7503e98d6efb012804495ab0bd7bb79b
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-10 11:40:51 +0900

    DOM要素の取得

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('aaa')
+class ProjectInput {
+  templateElemtnt: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElemtnt = document.getElementById('project-input') as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app') as HTMLDivElement;
+    const importedNode = document.importNode(this.templateElemtnt.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
