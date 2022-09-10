7764a19b99eb3c43ae9bf8776e252508577386fa
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-10 22:09:42 +0900

    DOM要素の操作

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,32 @@ class ProjectInput {
   templateElemtnt: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElemtnt = document.getElementById('project-input') as HTMLTemplateElement;
     this.hostElement = document.getElementById('app') as HTMLDivElement;
     const importedNode = document.importNode(this.templateElemtnt.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
