d268f75b8563e567bf2cb9bfff15a0bb04573231
HuntingRathalos <huntingandrathalos@gmail.com>
2022-09-15 09:43:42 +0900

    ファイルとフォルダの整理

---
+++ src/components/project-input.ts
@@ -0,0 +1,69 @@
+/// <reference path="base-component.ts"/>
+namespace App {
+  // ProjectList Class
+  export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
+    assignedProjects: Project[];
+
+    // super()の呼び出しが完了するまではthisは使えないため、type となる
+    constructor(private type: 'active' | 'finished') {
+      super('project-list', 'app', false, `${type}-projects`);
+      this.assignedProjects = [];
+      this.configure();
+      this.renderContent();
+    }
+    @autobind
+    dragOverHander(event: DragEvent) {
+      if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault();
+        const listEl = this.element.querySelector('ul')!;
+        listEl.classList.add('droppable');
+      }
+    }
+
+    @autobind
+    dropHander(event: DragEvent) {
+      const prjId = event.dataTransfer!.getData('text/plain');
+      projectState.moveProject(
+        prjId,
+        this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished
+      );
+    }
+
+    @autobind
+    dragLeaveHander(_: DragEvent) {
+      const listEl = this.element.querySelector('ul')!;
+      listEl.classList.remove('droppable');
+    }
+
+    // publicメソッドの定義が上
+    renderContent() {
+      const listId = `${this.type}-projects-list`;
+      this.element.querySelector('ul')!.id = listId;
+      this.element.querySelector('h2')!.textContent =
+        this.type === 'active' ? '実行中プロジェクト' : '完了プロジェクト';
+    }
+
+    configure() {
+      this.element.addEventListener('dragover', this.dragOverHander);
+      this.element.addEventListener('drop', this.dropHander);
+      this.element.addEventListener('dragleave', this.dragLeaveHander);
+      projectState.addListener((projects: Project[]) => {
+        const relevantProjects = projects.filter((prj) => {
+          if (this.type === 'active') {
+            return prj.status === ProjectStatus.Active;
+          }
+          return prj.status === ProjectStatus.Finished;
+        });
+        this.assignedProjects = relevantProjects;
+        this.renderProject();
+      });
+    }
+    private renderProject() {
+      const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+      listEl.innerHTML = '';
+      for (const prjItem of this.assignedProjects) {
+        new ProjectItem(listEl.id, prjItem);
+      }
+    }
+  }
+}
---
+++ src/state/project-state.ts
@@ -0,0 +1,56 @@
+namespace App {
+  // Project State Management
+  type Listener<T> = (items: T[]) => void;
+
+  class State<T> {
+    protected listeners: Listener<T>[] = [];
+    addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  export class ProjectState extends State<Project> {
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+      super();
+    }
+
+    static getInstace() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, manday: number) {
+      const newProject = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        manday,
+        ProjectStatus.Active
+      );
+      this.projects.push(newProject);
+      this.updataListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+      const project = this.projects.find((prj) => prj.id === projectId);
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updataListeners();
+      }
+    }
+
+    private updataListeners() {
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+  }
+
+  export const projectState = ProjectState.getInstace();
+}
